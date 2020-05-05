ab13d5bbe8d4c9ff5bcbafd76967e9096a4315e7
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-05-05 18:41:20 +0900

    [update]stylesheetの整理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:edit, :update, :create, :destroy]
   skip_before_action :admin_login_required
+
   def create
     @comment = @question.comments.build(comment_params)
     if user_signed_in?
@@ -51,48 +52,3 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:project_id, :subject_id, :part_id, :question_id, :content)
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-  # def edit
-  # end
-  #
-  # def update
-  #   if @comment.update(comment_params)
-  #     redirect_to current_user
-  #   else
-  #     render 'edit'
-  #   end
-  # end
-  #
-  # def destroy
-  #   @comment.destroy
-  #   redirect_back(fallback_location: root_path)
-  # end
-
-#   private
-#
-#   def set_parents
-#     @project = Project.find(params[:project_id])
-#     @subject = Subject.find(params[:subject_id])
-#     @part = Part.find(params[:part_id])
-#     @question = Question.find(params[:question_id])
-#   end
-#
-#   def set_comment
-#     @comment = Comment.find(params[:id])
-#   end
-#
-#   def comment_params
-#     params.require(:comment).permit(:question_id, :content)
-#   end
-# end
