0844eb21258a006ea134ced32e7093532f893f80
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-05-26 01:12:36 +0900

    [add]testの結果を確認できるページを追加

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,11 +1,19 @@
 class UserAnswersController < ApplicationController
   skip_before_action :admin_login_required
 
-  def new
-    @test = Test.find(1)
-    @test_questions = @test.test_questions
-    @user_answers = UserAnswer.new
+# スッキリしないコードだが正しく動く&付随してviewの方も曖昧な表現になっている（ある一つのtest_questionを取ってきてそこからtestを受けた全てのuserを取っている）
+  def index
+    @test = Test.find(params[:test])
+    @test_questions = TestQuestion.where(test_id: @test.id)
+    @test_question = TestQuestion.where(test_id: @test.id).first
+    @user_answers = UserAnswer.where(test_question_id: @test_question)
   end
+# 以下おそらく不要
+  # def new
+  #   @test = Test.find(params[:id])
+  #   @test_questions = @test.test_questions
+  #   @user_answers = UserAnswer.new
+  # end
 
   def create
     @user_answers = user_answers_params.values.each do |value|
@@ -16,6 +24,12 @@ class UserAnswersController < ApplicationController
       )
     end
     redirect_to current_user, notice: "テストが終了しました！お疲れ様です！"
+  end
+
+  def show
+    @user = User.find(params[:user])
+    @test = Test.find(params[:test])
+    @test_questions = TestQuestion.where(test_id: @test.id)
   end
 
   private
