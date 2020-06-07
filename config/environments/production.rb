3c873815df0e781c4db068fc6416e407b5ee4ac5
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-06-07 18:19:40 +0900

    init unicorn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'guarded-caverns-14017.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '54.168.106.1' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
