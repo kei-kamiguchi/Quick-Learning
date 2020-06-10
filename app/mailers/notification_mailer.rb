a1ed5803525cbf4eff0c91b1ec724c52e62ec7e0
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-06-10 21:54:04 +0900

    [fix]メールが送れないエラーの修正

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,13 +4,13 @@ class NotificationMailer < ApplicationMailer
   def comment_to_user_mail(comment)
     @comment = comment
 
-    mail to: "kei.kamiguchi517@gmail.com", subject: "#{@comment.admin.name}から返事が届いています！"
+    mail to: "#{@comment.admin.email}", subject: "#{@comment.admin.name}から返事が届いています！"
   end
 
   def comment_to_admin_mail(comment)
     @comment = comment
     @admins = Project.find_by(id: comment.question.part.subject.project_id).admin_participation_admins
-    mail to: "kei.kamiguchi517@gmail.com", subject: "#{@comment.user.name}から質問または返事が届いています！"
+    mail to: @admins.map(&:email).join(","), subject: "#{@comment.user.name}から質問または返事が届いています！"
   end
 
   def question_to_admin_mail(question)
